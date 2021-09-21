e99c188c8e8baee4a69f448d7c549bf45705dece
zeel <zeel02@gmail.com>
2021-09-21 13:30:45 -0400

    Initial style and layout of card sheet

--- scripts/MonarchCard.js
+++ scripts/MonarchCard.js
@@ -3,8 +3,26 @@ export default class MonarchCard extends CardConfig {
 		return foundry.utils.mergeObject(super.defaultOptions, {
 			template: "modules/monarch/templates/monarch-card.hbs",
 			classes: ["monarch", "monarch-card", "sheet"],
-			//width: 600,
+			width: 600,
+			height: "auto",
 			resizable: true
 		})
+	}
+
+	activateListeners(html) {
+		super.activateListeners(html);
+		html = html[0];
+
+		html.querySelector(".card-display").addEventListener("click", (event) => {
+			event.stopPropagation();
+			const cardDocumwnt = this.object;
+
+			new ImagePopout(cardDocumwnt.img, {
+				title: cardDocumwnt.data.name,
+				uuid: cardDocumwnt.data.uuid,
+				sharable: true,
+				editable: false
+			}).render(true);
+		});
 	}
 }
