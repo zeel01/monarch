80347e1bfd7ac468f0c0be7b9382346d8f1a6c53
zeel <zeel02@gmail.com>
2021-12-19 13:59:43 -0500

    Added template for pile

--- scripts/MonarchPile.js
+++ scripts/MonarchPile.js
@@ -3,7 +3,7 @@ import MonarchApplicationMixin from "./MonarchApplicationMixin.js";
 export default class MonarchPile extends MonarchApplicationMixin(CardsConfig) {
 	static get defaultOptions() {
 		return foundry.utils.mergeObject(super.defaultOptions, {
-			template: "modules/monarch/templates/monarch-deck.hbs",
+			template: "modules/monarch/templates/monarch-pile.hbs",
 			classes: ["monarch", "monarch-pile", "sheet"],
 			dragDrop: [{ dragSelector: "ol.cards li.card", dropSelector: "ol.cards" }],
 			width: 660,
